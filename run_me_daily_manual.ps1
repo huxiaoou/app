4c615fa1f900d872bd0e7cf8ce009aea829ed4dc
huxiaoou <516984451@qq.com>
2022-06-14 15:37:47 +0800

    Add daily run scripts

---
+++ run_me_daily_manual.ps1
@@ -0,0 +1,17 @@
+$trade_date = Read-Host -Prompt "Please input the report date, format = [YYYYMMDD]"
+python aux_download.py $trade_date
+python aux_intermediary.py $trade_date
+python aux_summary.py $trade_date
+python aux_margin.py $trade_date
+python aux_nav.py $trade_date
+python 00_input_order_inside.py $trade_date
+python 01_input_order_outside.py $trade_date
+python 02_traded_order.py $trade_date
+python 02_traded_order_agg.py $trade_date
+python 03_traded_order_summary.py $trade_date
+python 04_position_details.py $trade_date
+python 05_pnl_summary.py $trade_date
+python 06_risk_control.py $trade_date
+python 07_report.py $trade_date margin
+python 07_report.py $trade_date no_margin
+pause
